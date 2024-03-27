a0c640a03365d8447908b904e9599c6ec623e85c
ifelse023 <ifelse023@gmail.com>
2024-03-28 00:49:10 +0100

    ssh and so on

--- architect.sh
+++ architect.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+sudo pacman -S --needed --noconfirm wget curl base-devel mold sccache reflector python python-requests ccache lld paru rustup
 sudo usermod -a -G video,audio,network,sys,wheel wasd
 rustup default nightly
 sudo rsync -avh -av ~/architect/dotfiles/etc/ /etc/
@@ -8,8 +9,9 @@ python architect/main.py
 sudo python architect/files.py
 rsync -avh ~/architect/dotfiles/config/ ~/.config
 sudo mount -a
+sudo systemctl --user enable psd
 sudo systemctl disable bluetooth
 sudo systemctl mask bluetooth
 sudo keyd reload
-sudo systemctl --user enable psd
+bash ./ssh.sh
 chsh -s "$(which fish)"
---
+++ ssh.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mount_point="/mnt/usb_drive"
+
+if [ -b /dev/sda1 ]; then
+	if ! mount | grep -q /dev/sda1; then
+		mkdir -p $mount_point
+		sudo mount /dev/sda1 $mount_point
+	else
+		echo "/dev/sda1 is already mounted."
+		exit 1
+	fi
+else
+	echo "/dev/sda1 does not exist as a block device."
+	exit 1
+fi
+
+if [ -d "$mount_point/.ssh" ]; then
+	eval home=~$USER
+	sudo cp -rp $mount_point/.ssh "$home"
+	sudo chown -R $USER:$USER "$home/.ssh"
+	chmod 700 ~/.ssh
+	chmod 600 ~/.ssh/id_ed25519
+	chmod 644 ~/.ssh/id_ed25519.pub
+	echo ".ssh folder copied successfully."
+else
+	echo "No .ssh directory found on the USB drive."
+fi
+
+sudo umount $mount_point
+echo "USB drive unmounted."
