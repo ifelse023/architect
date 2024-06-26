3f3b399feda3703f14a518536d69ee0781a59251
ifelse023 <ifelse023@gmail.com>
2024-06-26 20:48:15 +0200

    :)

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -4,21 +4,13 @@ starship init fish | source
 if status is-interactive
 end
 
-
 set -gx EDITOR (which nvim)
 set -gx VISUAL $EDITOR
 set -gx SUDO_EDITOR $EDITOR
 
-set -Ux CC clang
-set -Ux CXX clang++
-set -Ux fish_user_paths /usr/lib/ccache $fish_user_paths
-set -U fish_user_paths ~/miniconda3/bin $fish_user_paths
 set -Ux EDITOR nvim
+set -Ux fish_user_paths /usr/lib/ccache $fish_user_paths
 set -gx RIPGREP_CONFIG_PATH /home/wasd/.config/.ripgreprc
-
-function ip --description 'Alias for ip with color'
-    command ip --color $argv
-end
 
 alias ls='eza --color=always --group-directories-first --icons' # preferred listing
 alias la='eza -a --color=always --group-directories-first --icons' # all files and dirs
@@ -38,6 +30,3 @@ alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
 alias ......='cd ../../../../..'
-
-# Created by `pipx` on 2024-05-19 13:18:18
-set PATH $PATH /home/wasd/.local/bin
