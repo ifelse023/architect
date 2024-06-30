3aa7a85c28575d6186f1f0bce300e8d88aa2b8cf
ifelse023 <ifelse023@gmail.com>
2024-06-30 02:45:11 +0200

    :)

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -5,7 +5,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-
+git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/plugins/autoupdate
 rsync -avz ~/architect/dotfiles/.zshrc ~/
 
 rsync -avz ~/architect/dotfiles/aliases.zsh ~/.oh-my-zsh/custom/
