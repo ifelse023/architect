7e1c62e5b3148c6cdee72ba8f5f1cc284b618c1c
ifelse023 <ifelse023@gmail.com>
2024-08-07 05:36:21 +0200

    yep

--- architect.sh
+++ architect.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-sudo pacman -S --needed --noconfirm wget curl base-devel mold sccache reflector python python-requests ccache lld rsync
+sudo pacman -S --needed --noconfirm wget curl base-devel mold sccache reflector python python-requests ccache lld rsync rustup
+rustup default nightly
 sudo usermod -a -G video,audio,network,sys,wheel wasd
 rsync -avh ~/architect/dotfiles/home/ ~/
 curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
@@ -9,9 +10,7 @@ sudo ./cachyos-repo.sh
 cd ..
 sudo pacman -S paru-bin
 paru -S alhp-keyring alhp-mirrorlist
-
 sudo rsync -rvh --no-perms --no-owner --no-group ~/architect/dotfiles/etc/ /etc/
-sudo pacman -Scc
 sudo pacman -Syu
 python architect/main.py
 sudo dosfslabel /dev/nvme0n1p1 BOOT
@@ -20,4 +19,6 @@ sudo mount -a
 sudo python architect/modify_files.py
 rsync -avh ~/architect/dotfiles/config/ ~/.config
 python ~/architect/architect/manage_directories.py
-paru -Scc --noconfirm
+paru -Scc
+sudo pacman -Rns $(pacman -Qtdq)
+sudo journalctl --vacuum-size=1M
