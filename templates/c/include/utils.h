4b389fdb51c5750058e902b00f3a7d23cc8eb736
ifelse023 <ifelse023@gmail.com>
2025-02-26 12:29:12 +0100

    :)

---
+++ templates/c/include/utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+/**
+ * @brief Adds two numbers
+ * @param a First number
+ * @param b Second number
+ * @return Sum of a and b
+ */
+int add(int a, int b);
+
+#endif /* UTILS_H */
