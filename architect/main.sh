5b520efe0425d9fdfc9472dd1bc2e48dea817d90
ifelse023 <ifelse023@gmail.com>
2025-04-25 23:43:12 +0200

    ..

--- architect/main.sh
+++ architect/main.sh
@@ -2,6 +2,23 @@
 
 main() {
 
+  curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
+  tar xvf cachyos-repo.tar.xz
+  cd cachyos-repo
+  sudo bash cachyos-repo.sh
+  cd ..
+  rm -rf ~/architect/cachyos-repo
+  rm ~/architect/cachyos-repo.tar.xz
+
+  sudo pacman -S paru-bin openssh python chezmoi sccache ccache libc++ clang --noconfirm --needed
+  chsh -s /usr/bin/fish
+
+  chezmoi init --apply --ssh git@github.com:ifelse023/dotfiles.git
+
+  sudo rsync -rvh --no-perms --no-owner --no-group ~/architect/config-files/etc/ /etc/
+
+  sudo pacman -Syu
+
   python ~/architect/architect/install_packages.py
 
   sudo dosfslabel /dev/nvme0n1p1 BOOT
