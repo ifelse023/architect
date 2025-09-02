cad2cb85c61b90f595b475c37ae99f946fc4f203
ifelse023 <162376926+ifelse023@users.noreply.github.com>
2025-09-02 21:01:33 +0200

    :)

--- architect/main.sh
+++ architect/main.sh
@@ -2,21 +2,27 @@
 set -e
 main() {
 
-  sudo pacman -S openssh rsync python curl wget mold --noconfirm --needed
-  curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
-  tar xvf cachyos-repo.tar.xz
-  cd cachyos-repo
-  sudo bash cachyos-repo.sh
-  cd ..
-  rm -rf ~/architect/cachyos-repo{,.tar.xz}
+  sudo pacman -Rns linux-firmware
+  sudo pacman -S linux-firmware-intel linux-firmware-whence rsync python curl wget --noconfirm --needed
 
-  sudo pacman -S paru-bin chezmoi sccache ccache libc++ clang dosfstools e2fsprogs --noconfirm --needed
+  sudo pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
+  sudo pacman-key --lsign-key F3B607488DB35A47
+
+  sudo pacman -U 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-keyring-20240331-1-any.pkg.tar.zst' \
+    'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-mirrorlist-22-1-any.pkg.tar.zst' \
+    'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-v3-mirrorlist-22-1-any.pkg.tar.zst' \
+    'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-v4-mirrorlist-22-1-any.pkg.tar.zst' \
+    'https://mirror.cachyos.org/repo/x86_64/cachyos/pacman-7.0.0.r7.g1f38429-1-x86_64.pkg.tar.zst'
+
+  sudo rsync -rvh --no-perms --no-owner --no-group ~/architect/config-files/etc/ /etc/
+  sudo pacman -Scc
+  sudo pacman -Syyu --noconfirm
+
+  sudo pacman -S paru-bin chezmoi sccache ccache libc++ clang dosfstools e2fsprogs mold --noconfirm --needed
   chsh -s /usr/bin/fish
 
   sudo rsync -rvh --no-perms --no-owner --no-group ~/architect/config-files/etc/ /etc/
   chezmoi init --apply --ssh git@github.com:ifelse023/dotfiles.git
-
-  sudo pacman -Syu
 
   python ~/architect/architect/install_packages.py
 
@@ -24,8 +30,6 @@ main() {
   sudo e2label /dev/nvme0n1p2 ROOT
 
   sudo mount -a
-
-  sudo journalctl --vacuum-size=1M
 
   sudo usermod -aG video,audio,network,git,wheel,input wasd
   python ~/architect/architect/service_manager.py --enable
@@ -36,6 +40,7 @@ main() {
 
   sudo pacman -Sc --noconfirm
   paru -Sc --noconfirm
+  sudo journalctl --vacuum-size=1M
   sudo pacman -R $(pacman -Qtdq)
 }
 
